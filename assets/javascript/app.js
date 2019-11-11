dfaa55dbfc4154de6c090b1cf35ba326477ad64c
naglaswangas <fernandosolisjr@gmail.com>
2019-11-10 23:30:45 -0600

    frontend

---
+++ assets/javascript/app.js
@@ -0,0 +1,84 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDrQZ4Sshlg0IK3JN0hp2Z0YfpM7C3Govw",
+    authDomain: "train-project-c47e3.firebaseapp.com",
+    databaseURL: "train-project-c47e3.firebaseio.com",
+    projectId: "train-project-c47e3",
+    storageBucket: "train-project-c47e3.appspot.com",
+    messagingSenderId: "301815028477"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  // Button for adding a train
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+
+    var trainName = $("#train-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var trainPlatform = $("#platform-input").val().trim();
+    var trainFirst= $("#first-train-input").val().trim();
+    var trainFrequency= $("#frequency-input").val().trim();
+
+  // Storing new trains
+    var newTrain = {
+      train: trainName,
+      destination: trainDestination,
+      platform: trainPlatform,
+      firstTrain: trainFirst,
+      frequency: trainFrequency
+    };
+  // Uploading new trains
+  database.ref().push(newTrain);
+  console.log(newTrain);
+  alert("Train added!");
+
+  // Clear fields so new train can be added  
+  
+  $("#train-input").val("")
+  $("#destination-input").val("")
+  $("#platform-input").val("")
+  $("#first-train-input").val("")
+  $("#frequency-input").val("")
+
+  });
+
+  // Calculate next train time
+function getTrainTime(t, f) {
+
+  var now = moment();
+  console.log(now);
+  
+  var first = moment(t, "HH:mm");
+  console.log(first);
+
+  if (moment(now).isBefore(first)) {
+    return [first.format("HH:mm"), (first.diff(now, 'minutes'))];
+  }
+
+  while (moment(now).isAfter(first)) {
+    first.add(f, 'm');
+  } 
+
+  return [first.format("HH:mm"), (first.diff(now, 'minutes'))];
+};
+
+  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+
+  var trainName = childSnapshot.val().train;
+  var trainDestination = childSnapshot.val().destination;
+  var trainPlatform = childSnapshot.val().platform;
+  var trainFirst = childSnapshot.val().firstTrain;
+  var trainFrequency = childSnapshot.val().frequency;
+
+  var trainArrival = getTrainTime(trainFirst, trainFrequency)[0];
+  var trainMinutesAway = getTrainTime(trainFirst, trainFrequency)[1];
+
+
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
+  trainPlatform + "</td><td>" + trainFirst + "</td><td>" + trainFrequency + "</td><td>" + trainArrival + "</td><td>" + trainMinutesAway + "</td></tr>");
+
+  
+  });
